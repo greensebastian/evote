55ed3b160f2555a64e468571e1d9bbb0d78dea2c
Olov Gunther <Olov Gunther>
2016-10-04 21:23:55 +0200

    Reduce space for the printed codes and remove instructions

--- actions/codeprint.php
+++ actions/codeprint.php
@@ -3,33 +3,38 @@
 function decode_str($string){
 	return iconv('UTF-8', 'windows-1252', $string);
 }
+
+
 if($evote->verifyUser($_SESSION["user"], 0)){
 	require("../fpdf/fpdf.php");
-	$pdf = new FPDF();
+	class PDF extends FPDF {
+		function Footer(){
+		    // Go to 1.5 cm from bottom
+		    $this->SetY(-15);
+		    // Select Arial italic 8
+		    $this->SetFont('Arial','I',8);
+		    // Print centered page number
+		    $this->Cell(0,10,'Sida '.$this->PageNo(),0,0,'C');
+		}
+	}
+	$pdf = new PDF();
+
 	$pdf->AddPage();
-	$pdf->SetFont('Courier','B',16);
+	$pdf->setFont('Arial', 'B', 30);
+	$pdf->MultiCell(190,10,'Koder ('.count($codes).' st)', 0, 'C');
+
+	$pdf->SetFont('Courier','',16);
 
 	$count = 0;
-	$instr_per_page = 3;
-	$instructions = decode_str(file_get_contents(__DIR__."/../data/code_instr.txt"));
+	$row = 1;
+	$codes_per_row = 6;
 	foreach($codes as $c){
 		$count ++;
-		$pdf->SetFont('Courier','B',16);
-		$title = decode_str("E-vote, E-sektionens röstningssystem ".$_SERVER['SERVER_NAME']);
-		$pdf->Ln();
-		$pdf->Cell(190,10,$title);
-		$pdf->Ln();
-		$pdf->SetFont('Arial','',8);
-		$pdf->Multicell(190, 4, $instructions);
-		$pdf->SetFont('Arial','',12);
-		$pdf->Ln();
-		$pdf->Multicell(190,10,decode_str("Din personliga kod är"), 0, 'C', false);
-		$pdf->SetFont('Times','B',16);
-		$pdf->SetFillColor(215, 215, 215);
-		$pdf->Multicell(190,10,$c, 0, 'C', true);
-		$pdf->Ln();
-		if($count % $instr_per_page == 0){
-			$pdf->AddPage();
+		$pdf->Cell(30,10,$c, 0, 'C');
+
+		if($count % $codes_per_row == 0){
+			$pdf->Ln();
+			$row ++;
 		}
 	}
 	$pdf->Output();
